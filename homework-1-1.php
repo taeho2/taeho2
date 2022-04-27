80c56be6389607d4c322abb2244ba13ada862b02
taeho2 <100827472+taeho2@users.noreply.github.com>
2022-04-28 02:44:49 +0900

    Delete homework-1-1.php

--- homework-1-1.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<div>정수</div>  #제목  
-    <form action="homework-1-2.php" method="get">
-    <input type="text" name="i" placeholder="Enter Number here">
-</body>
-</html>
